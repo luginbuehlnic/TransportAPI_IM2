f4b80e8e22e0eb21c9adeb14d3800eae26cb6e60
Nic Luginbuehl <70602291+luginbuehlnic@users.noreply.github.com>
2025-05-27 16:36:57 +0200

    display api data and css design

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,109 +1,147 @@
-const connection_Buttons = document.querySelector('#connections');
-const connection_name = document.querySelector('#name');
-const connection_duration = document.querySelector('#duration');
-const connection_from = document.querySelector('#from');
-const connection_platform = document.querySelector('#platform');
-const connection_arriveplt = document.querySelector('#arriveplt');
-const connection_to = document.querySelector('#to');
-const connection_departure = document.querySelector('#departure');
-const connection_arrival = document.querySelector('#arrival');
 const details = document.querySelector('#details');
+const stationButtons = document.querySelectorAll('.stationButton');
+const trainBoxContainer = document.querySelector('.trainBox');
+const loadingTrain = document.getElementById('loadingTrain');
+const connectionBox = document.querySelector('.connectionBox');
 
-const connections = [
-    {
-        from: 'Zürich HB',
-        to: 'Chur'
-    },
-    {
-        from: 'Chur',
-        to: 'Zürich HB'
-    },
-    {
-        from: 'St. Gallen',
-        to: 'Chur'
-    },
-    {
-        from: 'Chur',
-        to: 'St. Gallen'
-    }
-]
+let selectedFrom = '';
+let selectedTo = '';   
 
-async function init() {
-    details.style.display = 'none';
+stationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const id = button.id;
+        const isFrom = id.includes('From');
+        const group = isFrom ? 'from' : 'to';
+        const groupDiv = document.getElementById(group);
+        const stationName = button.innerText;
 
-    await initConnectionButtons()
-
-    // let result = await loadConnections('Chur', 'Zürich HB');
-    // console.log(result);
-
-    // connection_duration.innerText = formatDuration(result[0].duration);
-    // connection_name.innerText = result[0].products[0];
-}
-
-async function initConnectionButtons() {
-    if(!connection_Buttons) return;
-    details.style.display = 'none';
-    connection_Buttons.innerHTML = '';
-
-    for(const connection of connections) {
-        const button = document.createElement('button');
-        button.innerText = `${connection.from} - ${connection.to}`;
-        button.classList.add('connection-button');
-        button.addEventListener('click', async () => {
-            connection_from.innerText = connection.from;
-            connection_to.innerText = connection.to;
-
-            const connections = await loadConnections(connection.from, connection.to);
-            console.log(connections);
-
-            connection_duration.innerText = formatDuration(connections[0].duration);
-            connection_name.innerText = connections[0].products[0];
-            connection_platform.innerText = ` Gleis ${connections[0].from.platform}`;
-            connection_arriveplt.innerText = ` Gleis ${connections[0].to.platform}`;
-            connection_departure.innerText = new Date(connections[0].from.departure).toLocaleTimeString('de-DE', {
-                hour: '2-digit',
-                minute: '2-digit',
-                timeZone: 'Europe/Berlin',
-              });
-            connection_arrival.innerText = new Date(connections[0].to.arrival).toLocaleTimeString('de-DE', {
-                hour: '2-digit',
-                minute: '2-digit',
-                timeZone: 'Europe/Berlin',
-              });
-            
+        groupDiv.querySelectorAll('.stationButton').forEach(btn => {
+            btn.classList.remove('active');
         });
 
+        button.classList.add('active');
+        updateSelection(group, stationName);
+    });
+})
 
-        details.style.display = 'block';
-        connection_Buttons.appendChild(button);
+async function init() {
+
+}
+
+async function updateSelection(group, stationName) {
+    trainBoxContainer.innerHTML = '';
+    if(group === 'from') {
+        selectedFrom = stationName;
+    } else if(group === 'to') {
+        selectedTo = stationName;
+    }
+    
+    if(selectedFrom === selectedTo) {
+        sameStationWarning(selectedFrom);
+    } else if(selectedFrom && selectedTo) {
+        loadConnections(selectedFrom, selectedTo)
+    }
+}
+
+async function sameStationWarning(station) {
+    trainBoxContainer.innerHTML = '';
+    const warningBox = document.createElement('div');
+    warningBox.classList.add('warningBox');
+    warningBox.innerHTML = `
+        <h2>Bisch verwirrt?</h2>
+        <img id="questionIcon" src="img/QuestionIcon.svg" alt="Question Icon">
+        <h3>Du bisch scho in ${station} da gits kein Zug.</h3>
+        <p>Probiers namal Neu</p>
+    `;
+    trainBoxContainer.appendChild(warningBox);
+
+    if (trainBoxContainer) {
+        trainBoxContainer.scrollIntoView({ behavior: 'smooth' });
     }
 }
 
 async function loadConnections(from, to){
-    // let from = 'Chur',
-    //     to = 'Zürich HB';
-    const url = `http://transport.opendata.ch/v1/connections?from=${from}&to=${to}&limit=1`
+    const url = `http://transport.opendata.ch/v1/connections?from=${from}&to=${to}&limit=15`
+
+    loadingTrain.style.display = 'block';
 
     try {
         const response = await fetch(url);
         const data = await response.json();
-        const connections = data.connections;
-        return connections
+        const directConnections = data.connections.filter(conn => conn.transfers === 0);
+        if (directConnections.length >= 3) {
+            data.connections = directConnections.slice(0, 3);
+        } else {
+            data.connections = directConnections;
+        }
+        dispayConnections(data.connections);
+        
+        if (trainBoxContainer) {
+            trainBoxContainer.scrollIntoView({ behavior: 'smooth' });
+        }
     } catch (error) {
-        return false
         console.error('Error fetching data:', error);
+        return false
+    } finally {
+        loadingTrain.style.display = 'none';
     }
 }
 
-function formatDuration(duration) {
-    const [_, time] = duration.split('d');
-    const [hours, minutes] = time.split(':').map(Number);
-  
-    const parts = [];
-    if (hours) parts.push(`${hours} Std`);
-    if (minutes) parts.push(`${minutes} Min`);
-  
-    return parts.join(' ') || '0 Min';
-  }
+async function dispayConnections(connections) {
+    trainBoxContainer.innerHTML = '';
+    connections.forEach(conn => {
+        const fromTime = conn.from.departure.substring(11, 16);
+        const toTime = conn.to.arrival.substring(11, 16);
+        const fromStation = conn.from.station.name;
+        const toStation = conn.to.station.name;
+        const platform = conn.from.platform || '–';
+        const name = conn.products[0] || 'Unbekannt';
+        const parts = conn.duration.split('d')[1].split(':');
+        const hours = parseInt(parts[0], 10);
+        const minutes = parts[1];
+        const duration = `${hours}:${minutes}h`;
+
+        const box = document.createElement('div');
+        box.classList.add('singleBox');
+        box.innerHTML = `
+            <div class="topBox">
+                <div class="trainIcon">
+                    <img class="smallTrain" src="img/RunnerLogoPurple.svg" alt="Train Icon">
+                </div>
+                <div class="route">
+                    <p>${fromStation} - ${toStation}</p>
+                </div>
+                <div class="trainName">
+                    <p>${name}</p>
+                </div>
+            </div>
+            <div class="bottomBox">
+                <div class="platform">
+                    <p>Gleis ${platform}</p>
+                </div>
+                <div class="depart">
+                    <p>${fromTime}</p>
+                </div>
+                <div class="lineDot">
+                    <img src="img/LineDot.svg">
+                </div>
+                <div class="arrival">
+                    <p>${toTime}</p>
+                </div>
+            </div>
+            <div class="detailBox">
+                <p class="duration">${duration}</p>
+            </div>
+        `;
+        trainBoxContainer.appendChild(box);
+    })
+}
+
+async function formatDuration(duration) {
+    const parts = duration.split('d')[1].split(':');
+    const hours = parseInt(parts[0], 10);
+    const minutes = parts[1];
+    return `${hours}:${minutes}h`;
+}
 
 await init();
